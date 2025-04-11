a30a6438108e3cfba91cdb4e991a3a9e6fbc066c
Carlos Domingos <99830819+CarlosDevITT@users.noreply.github.com>
2025-04-11 14:16:09 -0400

    Faça seu pedido online agora Xis Querência do Sul
    Xis Querência do Sul Restaurant
    Serviços
    
        Delivery
        Mesas externas
        Pratos veganos
        Bom para assistir a esportes
    
    Informações
    
        Menu
        QUEM SOMOS
        Política de privacidade
        Termos de serviço
    
    Endereço
    
        R. Dr. José Bonifácio Malburg, 64 - Loja 02
        Centro, Itajaí - SC, 88301-350
        Menu
        Horário de funcionamento: Fechado ⋅ Abre às 18:30

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+window.onload = function() {
+    // Oculta a tela de carregamento
+    document.getElementById('loading-screen').style.display = 'none';
+    // Exibe o conteúdo principal
+    document.getElementById('main-content').style.display = 'block';
+};
